553a10f96a605faa107dd2c755e3fe5e74dfb6c6
Florian Weiss <floweiss6@gmail.com>
2021-03-04 17:32:45 +0100

    Add Bridge project

---
+++ SWP_Bridge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWP_Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

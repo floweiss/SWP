a57728832eae0d0b2903e2543bc6b6c992165171
Florian Weiss <floweiss6@gmail.com>
2021-03-11 17:27:49 +0100

    add command & composite project

---
+++ SWP_Command_Composite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWP_Command_Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
